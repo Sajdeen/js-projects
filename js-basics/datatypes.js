518cda776c5133b0d95c127e21829261a4f1c2f4
Sajdeen Rahmani <93546711+Sajdeen@users.noreply.github.com>
2024-06-27 16:05:42 +0000

    done

---
+++ js-basics/datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version
+//alert (3+5);
+
+
+console.log("sajdeen")
+
+let name ="Rahmani"
+let age = 90
+let isLoggedIn = false
+ let state;
+
+
+ //number => 2 to power 53
+ //bigint
+ //string => ""
+ //boolean => true/false
+ //null => standalone value
+ //undefined => value doesnt assign in variable thtsy it comes undefined
+ 
+ //symbol => unique
+
+
+ //object
+
+
+ console.log(typeof "sajdeen")
+ console.log(typeof age)
+
+ console.log(typeof null)//so null is object
+ console.log(typeof undefined) //undefined
