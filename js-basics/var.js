3f0acfd64346108253c58026cff886233c603a9e
Sajdeen Rahmani <93546711+Sajdeen@users.noreply.github.com>
2024-06-27 15:27:49 +0000

    done

--- js-basics/var.js
+++ js-basics/var.js
@@ -2,13 +2,19 @@ const accountId = 14452
 let accountEmail ="sajdeen012@gmail.com"
 var accountPassword = "12345"
 accountCity = "rajani"
-accountState =""
+let accountState;
 
 //accountId = 2
 
+
+/*
+Prefer not to use var because of issue in
+block scope and functional scope
+
+*/
 console.log(accountId);
 accountEmail ="sajen012@gmail.com"
 var accountPassword = "12345"
 accountCity = "kolkata"
 
-console.table([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
