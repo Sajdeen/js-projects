f7847183efaa9484e72b2fff4390bfe3953857f5
Sajdeen Rahmani <93546711+Sajdeen@users.noreply.github.com>
2024-06-27 15:20:14 +0000

    done

--- js-basics/var.js
+++ js-basics/var.js
@@ -2,7 +2,13 @@ const accountId = 14452
 let accountEmail ="sajdeen012@gmail.com"
 var accountPassword = "12345"
 accountCity = "rajani"
+accountState =""
 
 //accountId = 2
 
 console.log(accountId);
+accountEmail ="sajen012@gmail.com"
+var accountPassword = "12345"
+accountCity = "kolkata"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
