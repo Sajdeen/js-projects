bb9132bd6065a0119dc1ccf971bcc40ffffd2daa
Sajdeen Rahmani <93546711+Sajdeen@users.noreply.github.com>
2024-06-27 16:41:34 +0000

    done

---
+++ js-basics/datatype-conversion-confusion.js
@@ -0,0 +1,27 @@
+let score = undefined
+let age = "eight"
+console.log(typeof score);
+console.log (typeof (age));
+
+let valueInNumber = Number(score)
+console.log (typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" =>33
+//"99abc" => NaN (Not a number)
+//true => 1;false =>0
+
+let  isLoggedIn = "sajdeen"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"sajdeen" =>true 
+
+ let someNumber = 99
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+  console.log(typeof(stringNumber));
