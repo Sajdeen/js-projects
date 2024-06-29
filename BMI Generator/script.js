c51777828f8ebdcf37699bd8adee1a3c0de2d5bf
Sajdeen Rahmani <93546711+Sajdeen@users.noreply.github.com>
2024-06-29 21:34:55 +0530

    done

---
+++ BMI Generator/script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('form')
+//this usecase will give you empty
+//const height = parseInt(document.querySelector('#height').value)
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const results = parseInt(document.querySelector('#height').value)
+
+
+})
