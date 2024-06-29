877f062b1d32c72195ffd7e8920f6dbc15a16b6a
Sajdeen Rahmani <93546711+Sajdeen@users.noreply.github.com>
2024-06-29 21:41:01 +0530

    Done

--- BMI Generator/script.js
+++ BMI Generator/script.js
@@ -4,9 +4,12 @@ const form = document.querySelector('form')
 form.addEventListener('submit', function(e){
     e.preventDefault()
 
-    const height = parseInt(document.querySelector('#height').value)
-    const weight = parseInt(document.querySelector('#weight').value)
-    const results = parseInt(document.querySelector('#height').value)
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
 
-
-})
+if (height === '' || height < 0 || is NaN(height)){
+    results.innerHTML = `Please give a valid height ${height}`;
+}
+results.innerHTML = `${height}`;
+});
