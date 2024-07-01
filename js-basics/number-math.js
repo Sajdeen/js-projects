d2bcf47844f9095804037ed018ca62e37e1fa318
Sajdeen Rahmani <93546711+Sajdeen@users.noreply.github.com>
2024-07-01 19:15:04 +0000

    nums&maths.js

---
+++ js-basics/number-math.js
@@ -0,0 +1,20 @@
+const score = 400
+
+const balance = new Number (100)
+//console.log(balance);
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+//const otherNumber = 123.8986
+
+const hundred =100000
+
+//+++++++Maths+++++//
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.floor(42.0));
+
+console.log(Math.random()*7);
