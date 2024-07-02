872f330fdf5e2848e254b74db15f34662c4959fa
Sajdeen Rahmani <93546711+Sajdeen@users.noreply.github.com>
2024-07-02 06:02:31 +0000

    num&math.js

--- js-basics/number-math.js
+++ js-basics/number-math.js
@@ -17,4 +17,14 @@ console.log(Math.abs(-4));
 console.log(Math.round(4.6));
 console.log(Math.floor(42.0));
 
-console.log(Math.random()*7);
+console.log(Math.random(4.6)*7);
+
+//console.log(Math.random() * 10 + 1);
+
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min)
