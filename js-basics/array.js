46291b18710a19248a841b0433e6946ecb214ba5
Sajdeen Rahmani <93546711+Sajdeen@users.noreply.github.com>
2024-07-03 04:40:40 +0000

    array

---
+++ js-basics/array.js
@@ -0,0 +1,20 @@
+ const myArray =[0,1,2,3,4,5]
+ const myHeroes = ["shaktiman","naggur"]
+
+ const myArr4 = new Array(1,2,3,4)
+ console.log(myArray[0]);
+
+ //Array Methods
+
+ myArray.push(6)
+
+ myArray.push(7)
+ myArray.pop()
+ console.log(myArray);
+
+ //myArray.unshift(9);
+// myArray.shift()
+
+console.log(myArray.in);
+
+ console.log(myArray);
