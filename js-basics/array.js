986dc807589747ed3ce17a1583a97717ac2ad4d0
Sajdeen Rahmani <93546711+Sajdeen@users.noreply.github.com>
2024-07-03 06:21:40 +0000

    done

--- js-basics/array.js
+++ js-basics/array.js
@@ -15,6 +15,35 @@
  //myArray.unshift(9);
 // myArray.shift()
 
-console.log(myArray.in);
+//console.log(myArray.includes(9));
+//console.log(myArray.indexOf(3));
+
+
+//const newArr = myArray.join()
+//console.log(myArray);
+//console.log(newArr);
+
 
  console.log(myArray);
+
+ //slice,splice
+
+ const myn1 = myArray.slice(1,3)
+
+ console.log("A", myArray);
+ console.log(myn1);
+
+
+ console.log("B", myArray);
+ const myn2 = myArray.splice(1,3)
+console.log(myn2);
+
+const myn3 = myArray.splice(1,3)
+console.log("C", myArray);
+ 
+console.log(myn3);
+
+
+
+
+ 
