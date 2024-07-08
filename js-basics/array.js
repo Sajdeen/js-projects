3c89c3803c87bb3e1d8b83e750b3a78853f6e34d
Sajdeen Rahmani <93546711+Sajdeen@users.noreply.github.com>
2024-07-08 05:25:24 +0000

    done

--- js-basics/array.js
+++ js-basics/array.js
@@ -50,4 +50,22 @@ console.log(myn3);
 const marvel_heroes = ["thor","Ironman","batman"]
 const dc_heroes = ["superman", "flash", "batman"]
 
- 
+ marvel_heroes.push(dc_heroes)
+
+ //console.log(marvel_heroes);
+ //console.log(marvel_heroes[3][1]);
+
+ //const allHeros = marvel_heroes.concat(dc_heroes)
+ //console.log(allHeros);
+
+ const all_new_Heros = [...marvel_heroes, ...dc_heroes]
+
+ //console.log(all_new_Heros);
+
+ const  another_array = [1, 2, 3, [4,5,6], 6, [6,7, [4, 5]]];
+ const real_another_array = another_array.flat(Infinity)
+ console.log(real_another_array);
+
+ //const another_array = [1, 2, 3, [4, 5, 6], 6, [6, 7, [4, 5]]];
+//const real_another_array = another_array.flat(Infinity);
+//console.log(real_another_array);
