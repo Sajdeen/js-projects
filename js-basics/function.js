2aa06ec23b476a1d270636d59a6af3496182c3d1
sajdeen <sajdeen0786@gmail.com>
2024-07-21 00:53:26 +0530

    Done

---
+++ js-basics/function.js
@@ -0,0 +1,12 @@
+function sayMyName(){
+    console.log("S");
+  console.log("J");
+    console.log("D");
+
+   console.log("S");
+}
+//sayMyName()
+function addTwoNumbers(number1,number2){
+   console.log (number1 + number2);
+}
+addTwoNumbers(3,4)
