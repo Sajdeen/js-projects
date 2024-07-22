3725503db21ad69dace8eeaf310536896179b625
Sajdeen Rahmani <93546711+Sajdeen@users.noreply.github.com>
2024-07-22 13:36:30 +0530

    container config done

--- js-basics/var.js
+++ js-basics/var.js
@@ -4,15 +4,27 @@ let accountPassword = "12345";
 let accountCity = "rajani";
 let accountState;
 
+<<<<<<< HEAD
+//accountId = 2
+accountEmail = "hardhu@gmail.com"
+accountPassword = "_&&+(())"
+accountCity = "Kolkata"
+=======
 /*
 Prefer not to use var because of issues with
 block scope and functional scope.
 */
 
+>>>>>>> 2aa06ec23b476a1d270636d59a6af3496182c3d1
 console.log(accountId);
+<<<<<<< HEAD
+ l
+=======
 
 accountEmail = "sajen012@gmail.com";
 accountPassword = "12345";
 accountCity = "kolkata";
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+>>>>>>> 2aa06ec23b476a1d270636d59a6af3496182c3d1
