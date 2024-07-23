fb1aaa52a61ebbd0bf75e0eb53f590167100b17f
sajdeen <sajdeen0786@gmail.com>
2024-07-24 02:36:02 +0530

    Done

--- js-basics/function.js
+++ js-basics/function.js
@@ -33,4 +33,30 @@ return
 
 //console.log(loginUserMessage("sajdeen"))//O/p  sajdeen just logged in
 //console.log(loginUserMessage(""))// O/p just logged in
-console.log(loginUserMessage("sakinas"))//when we dont pass the value it comes undefined
+//console.log(loginUserMessage("sakinas"))//when we dont pass the value it comes undefined
+
+function calculateCartPrice(val1,val2,...num1){
+   return num1
+}
+//console.log(calculateCartPrice(200,400,520,5222))
+
+const user = {
+   username: "sajdeen",
+   price:199
+}
+function handleObject(anyobject){
+   console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//handleObject(user)
+handleObject({
+   username: "sajdeen",
+   price:199
+})
+
+const myNewArray = [200, 400, 5000, 600]
+function returnSecondValue(getArray){
+   return getArray[2]
+}
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,5000,600]));
+
