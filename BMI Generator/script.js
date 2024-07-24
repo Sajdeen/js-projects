b1a46b8fcee3070ee07e6ba9a02ba9ac43c4f766
Sajdeen Rahmani <93546711+Sajdeen@users.noreply.github.com>
2024-07-24 11:15:34 +0530

    Done

--- BMI Generator/script.js
+++ BMI Generator/script.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 const form = document.querySelector('form')
 //this usecase will give you empty
 //const height = parseInt(document.querySelector('#height').value)
@@ -10,9 +8,9 @@ form.addEventListener('submit', function(e){
     const weight = parseInt(document.querySelector('#weight').value);
     const results = document.querySelector('#results');
 
-if (height === '' || height < 0 || is NaN(height)){
+if (isNaN(height) === '' || height <= 0 || isNaN (height)){
     results.innerHTML = `Please give a valid height ${height}`;
-}else if(weight === '' || weight< 0 || isNan(weight)){
+}else if(isNan(weight) === '' || weight< 0 || isNan(weight)){
     results.innerHTML = `Please give a valid height ${height}`;
 }else{
  const bmi = (weight / ((height*height)/10000)).toFixed(2)
@@ -33,8 +31,7 @@ if (height === '' || height < 0 || is NaN(height)){
     results.innerHTML = `Please give a valid height ${height}`;
 }
 results.innerHTML = `${height}`;
->>>>>>> dd941e417c986df5e614e2b874a0c94df00a0dad
-=======
+
 const form = document.querySelector('form')
 //this usecase will give you empty
 //const height = parseInt(document.querySelector('#height').value)
@@ -49,5 +46,4 @@ if (height === '' || height < 0 || isNaN (height)){
     results.innerHTML = `Please give a valid height ${height}`;
 }
 results.innerHTML = `${height}`;
->>>>>>> c7c69b313796a652e44409a2fb7304bf64315369
-});
+))
