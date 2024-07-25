6e749db23f95e297a7cfef8b44b1c7b10428d66c
sajdeen <sajdeen0786@gmail.com>
2024-07-25 23:02:04 +0530

    Dne

--- BMI Generator/script.js
+++ BMI Generator/script.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 const form = document.querySelector('form')
 //this usecase will give you empty
 //const height = parseInt(document.querySelector('#height').value)
@@ -10,44 +8,8 @@ form.addEventListener('submit', function(e){
     const weight = parseInt(document.querySelector('#weight').value);
     const results = document.querySelector('#results');
 
-if (height === '' || height < 0 || is NaN(height)){
+if (height === '' || height < 0 || isNaN(height)){
     results.innerHTML = `Please give a valid height ${height}`;
-}else if(weight === '' || weight< 0 || isNan(weight)){
+}else if(weight === '' || weight< 0 || isNan(weight))
     results.innerHTML = `Please give a valid height ${height}`;
-}else{
- const bmi = (weight / ((height*height)/10000)).toFixed(2)
- //show the result
- results.innerHTML =<span>${bmi}</span>
-=======
-const form = document.querySelector('form')
-//this usecase will give you empty
-//const height = parseInt(document.querySelector('#height').value)
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-
-    const height = parseInt(document.querySelector('#height').value);
-    const weight = parseInt(document.querySelector('#weight').value);
-    const results = document.querySelector('#results');
-
-if (height === '' || height < 0 || is NaN(height)){
-    results.innerHTML = `Please give a valid height ${height}`;
-}
-results.innerHTML = `${height}`;
->>>>>>> dd941e417c986df5e614e2b874a0c94df00a0dad
-=======
-const form = document.querySelector('form')
-//this usecase will give you empty
-//const height = parseInt(document.querySelector('#height').value)
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-
-    const height = parseInt(document.querySelector('#height').value);
-    const weight = parseInt(document.querySelector('#weight').value);
-    const results = document.querySelector('#results');
-
-if (height === '' || height < 0 || isNaN (height)){
-    results.innerHTML = `Please give a valid height ${height}`;
-}
-results.innerHTML = `${height}`;
->>>>>>> c7c69b313796a652e44409a2fb7304bf64315369
-});
+};
