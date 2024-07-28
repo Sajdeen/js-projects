a9415c0da7ff663ee003a0447cf34541cdd5c236
sajdeen <sajdeen0786@gmail.com>
2024-07-28 11:32:26 +0530

    Done

--- Guess the number/script.js
+++ Guess the number/script.js
@@ -5,3 +5,42 @@ const userInput = document.querySelector('#guessfield')
 const guessSlot = document.querySelector('.guesses')
 const remaining = document.querySelector('.lastResult')
 const lowOrHi = document.querySelector('.lowOrHi')
+const startOver = document.querySelector('.resultParas');
+
+const p = document.createElement('p')
+
+let prevGuess =[]
+let numGuess = 1;
+
+let playGame = true;
+
+
+if(playGame){
+    submit.addEventListener('Click', function(e){
+        e.preventDefault()
+        parseInt(userInput.value)
+    })
+}
+function validateGuess(guess){
+    //
+}
+
+function checkGuess(guess){
+    //
+}
+
+function displayGuess(guess){
+    //
+    }
+function displayMessage(message){
+//
+}
+
+function endGame(){
+    //
+}
+
+function newGame(){
+    //
+}
+
