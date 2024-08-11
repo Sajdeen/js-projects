38b8cd10a5ce36da3fc6ca16ad823e26de2eab11
sajdeen <sajdeen0786@gmail.com>
2024-08-11 12:09:05 +0530

    content config

---
+++ js-basics/controlflow.js
@@ -0,0 +1,32 @@
+//if
+
+/*const isUserLoggedIn = true
+
+const temperature = 41
+if( temperature ===40){
+    console.log("less than 50");
+}else{
+    console.log("temperature is greater than 50");
+}
+
+console.log("Execute");*/
+
+/*if(3!=2){
+console.log("executed");
+} */                               
+if(2=="2"){
+    console.log("executed");
+    } 
+//<,>,<=,>=,==,!=,===,!==
+
+const score = 200
+
+/*if(score > 100){
+  let power = "fly"
+    console.log(`User power: ${power}`);
+}
+//console.log(`User power: ${power}`);
+*/
+const balance = 1000
+
+//if(balance > 500) console.log("test"),console.log("test2");
