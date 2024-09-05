91fdda47f6b940095608b953e7f8ccbfa54e2387
sajdeen <sajdeen0786@gmail.com>
2024-09-05 12:36:30 +0530

    Done

--- js-basics/highordarry.js
+++ js-basics/highordarry.js
@@ -1,51 +1,36 @@
-// for of
+//for of
 
-["", "" , ""]
-//[{}, {},{}]
+//["", "", ""]
+//[{}, {}, {}]
 
-//const arr = [1,2,3,4,5]
+const arr =[1,2,3,4,5]
 
-//for (const num of arr) {
-   // console.log(num);
-//}
-
-const greetings = "hello World"
-for (const greet of greetings){
-    console.log(`Each char is ${greet}`);
+for(const num of arr){
+console.log(num);
 }
 
-//const greet ="happy Birthday"
-for( const gret of greet){
-    //console.log(`${gret}`);
-}
-// Array examples
-const arr = [1, 2, 3, 4, 5];
-
-for (const num of arr) {
-    console.log(num);
+const greetings ="hello world!"
+for(const greet of greetings){
+    console.log(`Each char is ${greet}`)
 }
 
-const greetings = "hello World";
-for (const greet of greetings) {
-    console.log(`Each char is ${greet}`);
+//Maps
+const map = new Map()
+map.set('IN', "INDIA")
+map.set('USA', "UNITED STATES PF AMERICA")
+map.set('FR', "FRANCE")
+
+console.log(map);
+for(const [key, value] of map){
+//console.log(key, ':-', value);
 }
 
-const greet = "Happy Birthday"; // Define greet before using it
-for (const gret of greet) {
-    console.log(`${gret}`);
+const myObject ={
+    game1:'NFS',
+    game2:'SPIDERMAN'
 }
 
-// Map example
-/*const map = new Map(); // Correct initialization
-map.set('IN', "INDIA");
-map.set('USA', "UNITED STATES OF AMERICA");
-map.set('FR', "FRANCE");
+for (const[key, value] of myObject ){
+    console.log(key, ':-', value);
+}
 
-console.log(map); // Correct case*/
-
-//Map
-const map: new Map()
-map.set('In', "INDIA");
-map.set('USA', "UNITEDs STATES OF AMERICA")
-    map.set('FR', "FRANCE")
- console.LOG(map);
