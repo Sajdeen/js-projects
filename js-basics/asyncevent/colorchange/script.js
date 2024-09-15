c6d43232e9eebb202c01b6800cea7e6cfef0294e
sajdeen <sajdeen0786@gmail.com>
2024-09-16 02:04:01 +0530

    done

---
+++ js-basics/asyncevent/colorchange/script.js
@@ -0,0 +1,10 @@
+//generate a random color
+
+const randomcolor = function(){
+    const hex ="0123456789ABCDEF"
+    let color = '#'
+    for (let i = 0; i<6; i++){
+
+    }
+};
+console.log(Math.random() * 16);
