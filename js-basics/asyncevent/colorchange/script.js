cdfd55ce6bceb47ce583702b3834ff1232768b21
sajdeen <sajdeen0786@gmail.com>
2024-09-21 02:05:56 +0530

    done

--- js-basics/asyncevent/colorchange/script.js
+++ js-basics/asyncevent/colorchange/script.js
@@ -4,7 +4,11 @@ const randomcolor = function(){
     const hex ="0123456789ABCDEF"
     let color = '#'
     for (let i = 0; i<6; i++){
-
+        color += hex[Math.floor(Math.random () * 16)]
     }
+    return color;
 };
-console.log(Math.random() * 16);
+
+const startChangingColor = function(){};
+
+document.querySelector("#Start").addEventListener('click', startChangingColor)
