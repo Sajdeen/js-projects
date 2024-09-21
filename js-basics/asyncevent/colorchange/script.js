8af370d45c422629a1a499688f1ec2d0090725ed
sajdeen <sajdeen0786@gmail.com>
2024-09-21 23:07:59 +0530

    Done

--- js-basics/asyncevent/colorchange/script.js
+++ js-basics/asyncevent/colorchange/script.js
@@ -1,14 +1,31 @@
 //generate a random color
 
 const randomcolor = function(){
-    const hex ="0123456789ABCDEF"
-    let color = '#'
-    for (let i = 0; i<6; i++){
-        color += hex[Math.floor(Math.random () * 16)]
-    }
-    return color;
+    const hex ="0123456789ABCDEF";
+    let color = '#';
+   for(let i = 0; i < 6; i++){
+   color += hex[Math.floor(Math.random() * 16)];
+   }
+   return color
 };
 
-const startChangingColor = function(){};
+let intervalId;
+const startChangingColor = function(){
 
-document.querySelector("#Start").addEventListener('click', startChangingColor)
+    if(!intervalId){
+  intervalId =  setInterval(changeBgColor, 1000);
+    }
+    function changeBgColor(){
+   document.body.style.backgroundColor = randomcolor();
+    }
+};
+const stopChangingColor = function(){
+    if(intervalId){
+    clearInterval(intervalId);
+    intervalId = null;
+    }
+};
+
+document.getElementById('start').addEventListener('click', startChangingColor);
+
+document.getElementById('stop').addEventListener('click', stopChangingColor);
