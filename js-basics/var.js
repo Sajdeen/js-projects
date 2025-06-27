12ea79f9f9698d0e39ec68aef754a0576d5f3ef3
sajdeen <sajdeen0786@gmail.com>
2025-06-27 18:33:33 +0530

    done

--- js-basics/var.js
+++ js-basics/var.js
@@ -1,30 +1,15 @@
 const accountId = 14452;
 let accountEmail = "sajdeen012@gmail.com";
 let accountPassword = "12345";
-let accountCity = "rajani";
+var accountCity = "rajani";
 let accountState;
 
-<<<<<<< HEAD
-//accountId = 2
-accountEmail = "hardhu@gmail.com"
-accountPassword = "_&&+(())"
-accountCity = "Kolkata"
-=======
+//accountId= 2 //not allowed as it is a constant
 /*
-Prefer not to use var because of issues with
-block scope and functional scope.
+Prefer not to use var, because of issue in block scope and functional scope.
 */
 
->>>>>>> 2aa06ec23b476a1d270636d59a6af3496182c3d1
-console.log(accountId);
-<<<<<<< HEAD
- l
-=======
 
-accountEmail = "sajen012@gmail.com";
-accountPassword = "12345";
-accountCity = "kolkata";
-
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
-
->>>>>>> 2aa06ec23b476a1d270636d59a6af3496182c3d1
+accountEmail = "sjlkl@gmail.com";
+accountCity = "Hyderabad";
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
