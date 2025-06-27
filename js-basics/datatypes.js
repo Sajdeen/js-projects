ac3367bd6757f1ef3fe4814597ffc2d04aa0f5f1
sajdeen <sajdeen0786@gmail.com>
2025-06-27 20:23:22 +0530

    done

--- js-basics/datatypes.js
+++ js-basics/datatypes.js
@@ -1,5 +1,5 @@
 "use strict"; //treat all js code as newer version
-//alert (3+5);
+//alert (3+5);we are not using alert in nodejs
 
 
 console.log("sajdeen")
@@ -14,7 +14,7 @@ let isLoggedIn = false
  //string => ""
  //boolean => true/false
  //null => standalone value
- //undefined => value doesnt assign in variable thtsy it comes undefined
+ //undefined => value doesn't assign in variable thtsy it comes undefined
  
  //symbol => unique
 
