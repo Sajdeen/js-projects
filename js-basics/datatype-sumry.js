15d0fe0cb69c5127bd00cc2fa568ed5947d191bd
sajdeen <sajdeen0786@gmail.com>
2025-06-28 21:01:50 +0530

    datatype-sumry

--- js-basics/datatype-sumry.js
+++ js-basics/datatype-sumry.js
@@ -5,11 +5,15 @@
 const score  =100
 const scoreValue = 100.3
 
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
 const id =Symbol('123')
 const anotherId = Symbol('123')
 console.log(id === anotherId); //false, because symbols are unique
 
-const bigNumber = 1234567890n//BigInt
+//const bigNumber = 1234567890n//BigInt
 
 
 //reference type(Non Primitive)
@@ -27,3 +31,15 @@ const myFunction = function(){
     console.log("hello world");
 
 }
+console.log(typeof bigNumber);
+
+console.log(typeof outsideTemp);
+console.log(typeof score);
+console.log(typeof scoreValue);
+ console.log(typeof myFunction);
+console.log(typeof heros);
+console.log(typeof myObj);
+console.log(typeof isLoggedIn);
+console.log(typeof userEmail); //undefined, because userEmail is declared but not assigned a value
+console.log(typeof id); //Symbol
+console.log(typeof anotherId); //Symbol
