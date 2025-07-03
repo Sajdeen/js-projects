ebe5e1642ddb30d0e5e5d125e2c178ba0890d03d
sajdeen <sajdeen0786@gmail.com>
2025-07-03 21:20:21 +0530

    array done

--- js-basics/array.js
+++ js-basics/array.js
@@ -10,7 +10,7 @@
 
  myArray.push(7)
  myArray.push(8)
- myArray.pop()// in pop we do not need value, it will remove the last element
+ myArray.pop()// in pop we do not need value
  console.log(myArray);
 
  //myArray.unshift(9);
@@ -51,26 +51,53 @@ console.log(myn3);
 const marvel_heroes = ["thor","Ironman","batman"]
 const dc_heroes = ["superman", "flash", "batman"]
 
- marvel_heroes.push(dc_heroes)
 
- console.log(marvel_heroes);
+// const allHeros= marvel_heroes.concat(dc_heroes)
+// console.log(allHeros);
+
+//instead of concat we can use spread operator
+const all_new_heros = [...marvel_heroes,...dc_heroes]
+console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+const real_another_array = another_array.flat(Infinity);
+console.log(real_another_array);
 
 
 
- //console.log(marvel_heroes);
- //console.log(marvel_heroes[3][1]);
+console.log(Array.isArray("sajda"))
+console.log(Array.from("sajda"))
+console.log(Array.from({name:"sajda"}))//inteeresting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300    
+ console.log(Array.of(score1, score2, score3)); // Creates an array with the given elements 
+
+
+// const marvel_heroes = ["thor","Ironman","batman"]
+// const dc_heroes = ["superman", "flash", "batman"]
+
+//  marvel_heroes.push(dc_heroes)
+
+//  console.log(marvel_heroes);
+
+
+
+//  console.log(marvel_heroes);
+//  console.log(marvel_heroes[3][1]);
 
  //const allHeros = marvel_heroes.concat(dc_heroes)
- //console.log(allHeros);
+//  console.log(allHeros);
 
- const all_new_Heros = [...marvel_heroes, ...dc_heroes]
+//  const all_new_Heros = [...marvel_heroes, ...dc_heroes]
 
- //console.log(all_new_Heros);
+//  //console.log(all_new_Heros);
 
- const  another_array = [1, 2, 3, [4,5,6], 6, [6,7, [4, 5]]];
- const real_another_array = another_array.flat(Infinity)
- console.log(real_another_array);
+//  const  another_array = [1, 2, 3, [4,5,6], 6, [6,7, [4, 5]]];
+//  const real_another_array = another_array.flat(Infinity)
+//  console.log(real_another_array);
 
- //const another_array = [1, 2, 3, [4, 5, 6], 6, [6, 7, [4, 5]]];
-//const real_another_array = another_array.flat(Infinity);
-//console.log(real_another_array);
+//  //const another_array = [1, 2, 3, [4, 5, 6], 6, [6, 7, [4, 5]]];
+// //const real_another_array = another_array.flat(Infinity);
+// //console.log(real_another_array);
