726c0d0524f270ce74fc381175584bdb5cb3502e
sajdeen <sajdeen0786@gmail.com>
2025-07-18 02:23:39 +0530

    Done

--- BMI Generator/script.js
+++ BMI Generator/script.js
@@ -1,35 +1,60 @@
-const form = document.querySelector('form')
-//const height = parseInt(document.querySelector('height').Value)
-//this usecase will give u empty
+// const form = document.querySelector('form')
+// //const height = parseInt(document.querySelector('height').Value)
+// //this usecase will give u empty
+
+// form.addEventListener('submit', function(e){
+//     e.preventDefault()
+
+//     const height= parseInt(document.querySelector('#height').value)
+//     const weight= parseInt(document.querySelector('#weight').value)
+//     const results = document.querySelector('#results')
+
+//     if(height === '' || height < 0 || isNaN(height)) {
+// results.innerHTML = `Please give a valid height ${height}`;
+//     }else
+//     if(weight === '' || weight < 0 || isNaN(weight)) {
+//         results.innerHTML = `Please give a valid weight ${weight}`;
+//             }else{
+//             const bmi = (weight/ ((height*height)/10000)).toFixed(2);
+
+//             let bmicategory = '';
+//             if(bmi < 18.6){
+//                 bmicategory = 'Underweight';
+//             }else if (bmi >= 18.6 && bmi <= 24.9){
+//                 bmicategory = 'Normal Range';
+//             }else{
+//                 bmicategory ='Overweight';
+//             }
+        
+//            //show the result
+//            results.innerHTML = `<span>Your BMI is ${bmi} (${bmicategory})</span>`
+//         }
+
+
+//    // results.innerHTML = `${height}`;
+// });
+const form = document.querySelector("form");
+//const height = document.querySelector("height").value;
+
 
 form.addEventListener('submit', function(e){
-    e.preventDefault()
+    e.preventDefault();
 
-    const height= parseInt(document.querySelector('#height').value)
-    const weight= parseInt(document.querySelector('#weight').value)
-    const results = document.querySelector('#results')
+const height = parseInt(document.querySelector("#height").value)
+const weight =parseInt(document.querySelector("#weight").value)
+const results = document.querySelector('#results');
 
-    if(height === '' || height < 0 || isNaN(height)) {
-results.innerHTML = `Please give a valid height ${height}`;
-    }else
-    if(weight === '' || weight < 0 || isNaN(weight)) {
-        results.innerHTML = `Please give a valid weight ${weight}`;
-            }else{
-            const bmi = (weight/ ((height*height)/10000)).toFixed(2);
-
-            let bmicategory = '';
-            if(bmi < 18.6){
-                bmicategory = 'Underweight';
-            }else if (bmi >= 18.6 && bmi <= 24.9){
-                bmicategory = 'Normal Range';
-            }else{
-                bmicategory ='Overweight';
-            }
+    if(height === '' || height < 0 || isNaN(height)){
+        results.innerHTML =`please give valid height{height}`;
         
-           //show the result
-           results.innerHTML = `<span>Your BMI is ${bmi} (${bmicategory})</span>`
-        }
-
-
-   // results.innerHTML = `${height}`;
+    }else if
+        (weight === '' || weight < 0 || isNaN(weight)){
+        results.innerHTML =`please give valid weight{weight}`;
+         }else
+         {
+       const bmi = (weight / ((height * height)/10000)).toFixed(2)
+            //show the results
+            results.innerHTML = `<span>${bmi}</span>`;
+         }
+    
 });
--- js-basics/truthy.js
+++ js-basics/truthy.js
@@ -23,3 +23,11 @@ const emptyObj = {}
 if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
 }
+
+//Nullish  coalescing operator (??): null undefined
+
+let val1;
+val1 = 5    ?? 10
+
+val1 = null
+console.log(val1); // If val1 is null or undefined, it will return 10
