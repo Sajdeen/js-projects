6c452218cce6d94a98a70875e83ef275798f78d9
sajdeen <sajdeen0786@gmail.com>
2025-07-19 16:38:27 +0530

    done

--- BMI Generator/script.js
+++ BMI Generator/script.js
@@ -1,39 +1,3 @@
-//  const form = document.querySelector("form");
-// // //const height = document.querySelector("height").value;
-
-
-// form.addEventListener('submit', function(e){
-//     e.preventDefault();
-
-//  const height = parseInt(document.querySelector("#height").value)
-//  const weight =parseInt(document.querySelector("#weight").value)
-//  const results = document.querySelector('#results');
-
-//      if(height === '' || height < 0 || isNaN(height)){
-//          results.innerHTML =`please give valid height{height}`;
-        
-//      }else if
-//         (weight === '' || weight < 0 || isNaN(weight)){
-//        results.innerHTML =`please give valid weight{weight}`;
-//         }else
-//           {
-//         const bmi = (weight / ((height * height)/10000)).toFixed(2)
-//             //show the results
-//             let bmicategory = "";
-
-//             if (bmi < 18.6){
-//                bmicategory = "Under Weight";
-//             }else if(bmi >= 18.6 && bmi <= 24.6){
-//                 bmicategory ="Normal Range";
-//             }else{
-//                 bmicategory =   "Over Weight";
-//             }
-
-            
-//             results.innerHTML = `<span>Your Bmi is ${bmi}(${bmicategory})</span>`;
-//          }
-    
-//  });
 const form = document.querySelector("form");
 
 form.addEventListener('submit', function(e){
